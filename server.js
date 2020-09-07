4ab1b9216536a8bdd2752879cfd4a267ce430799
Y. Violet Guo <violetguos@users.noreply.github.com>
2020-09-07 15:25:33 -0300

    implemented GET

--- server.js
+++ server.js
@@ -1,64 +1,73 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const googleSheets = require('gsa-sheets');
+const express = require("express");
+const bodyParser = require("body-parser");
+const googleSheets = require("gsa-sheets");
 
-const key = require('./privateSettings.json');
+const key = require("./privateSettings.json");
 
 // TODO(you): Change the value of this string to the spreadsheet id for your
 // GSA spreadsheet. See HW5 spec for more information.
-const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
+const SPREADSHEET_ID = "1MzJ2LzV2dDCwRiEXoE1kRuW6fXuJO9W9tYynl9Dsrys";
 
 const app = express();
 const jsonParser = bodyParser.json();
 const sheet = googleSheets(key.client_email, key.private_key, SPREADSHEET_ID);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 async function onGet(req, res) {
   const result = await sheet.getRows();
   const rows = result.rows;
-  console.log(rows);
+  // parse headers
+  const headers = rows[0];
 
-  // TODO(you): Finish onGet.
+  const resAsJson = [];
+  // start i at 1 to skip header row
+  for (let i = 1; i < rows.length; i++) {
+    const entry = {};
+    for (let j = 0; j < headers.length; j++) {
+      entry[headers[j]] = rows[i][j];
+    }
+    resAsJson.push(entry);
+  }
 
-  res.json( { status: 'unimplemented'} );
+  res.json(resAsJson);
 }
-app.get('/api', onGet);
+
+app.get("/api", onGet);
 
 async function onPost(req, res) {
   const messageBody = req.body;
 
   // TODO(you): Implement onPost.
 
-  res.json( { status: 'unimplemented'} );
+  res.json({ status: "unimplemented" });
 }
-app.post('/api', jsonParser, onPost);
+app.post("/api", jsonParser, onPost);
 
 async function onPatch(req, res) {
-  const column  = req.params.column;
-  const value  = req.params.value;
+  const column = req.params.column;
+  const value = req.params.value;
   const messageBody = req.body;
 
   // TODO(you): Implement onPatch.
 
-  res.json( { status: 'unimplemented'} );
+  res.json({ status: "unimplemented" });
 }
-app.patch('/api/:column/:value', jsonParser, onPatch);
+app.patch("/api/:column/:value", jsonParser, onPatch);
 
 async function onDelete(req, res) {
-  const column  = req.params.column;
-  const value  = req.params.value;
+  const column = req.params.column;
+  const value = req.params.value;
 
   // TODO(you): Implement onDelete.
 
-  res.json( { status: 'unimplemented'} );
+  res.json({ status: "unimplemented" });
 }
-app.delete('/api/:column/:value',  onDelete);
-
+app.delete("/api/:column/:value", onDelete);
 
 // Please don't change this; this is needed to deploy on Heroku.
 const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-  console.log(`Server listening on port ${port}!`);
+  console.log(`CS193X: Server listening on port ${port}!`);
 });
